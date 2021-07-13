0275447d0d65ac7ee9273587821e1a85b6ffa9ef
Ahmet Seha <sehabi.com@gmail.com>
2021-07-13 15:51:52 +0300

    unnecessary comment removed

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -25,13 +25,6 @@ function CardContainer() {
     setResult(e.target.value);
   };
 
-  // useEffect(() => {
-  //   const results = users.filter((res) =>
-  //     res.firstName.toLowerCase().includes(result)
-  //   );
-  //   setUsers(results);
-  // }, [result]);
-
   const filtered = users.filter((item) => {
     return Object.keys(item).some((key) => {
       return item[key]
